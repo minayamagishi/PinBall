442ebb783161ff8a83090d5879f46715707a2174
minayamagishi <yamagishi@change-jp.com>
2017-12-15 22:20:05 +0900

    PinBallプロジェクト　初めてのコミット
    課題提出のための質問用にアップします。

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ScoreController : MonoBehaviour
+{
+
+    //スコアを保持するための関数
+    private int score = 0;
+
+    //スコア表示するテキスト
+    private GameObject ScoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        // タグによって得点を変える
+        if (tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.score += 30;
+        }
+    else if (tag == "LargeCloudTag")
+        {
+            this.score += 40;
+        }
+
+    //ScorerTextにスコアを表示
+    this.ScoreText.GetComponent<Text>().text = Convert.ToString(score);
+
+    }
+}
